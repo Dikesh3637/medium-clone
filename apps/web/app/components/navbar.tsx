4f5bb7750763b0b156de9e26fd1f579a60ec89e2
Dikesh3637 <dikeshreddy.dev@gmail.com>
2024-07-02 02:59:08 +0530

    added the hero img to the homepage

--- apps/web/app/components/navbar.tsx
+++ apps/web/app/components/navbar.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 
 export const Navbar = () => {
     return (
-        <div className="home-navbar flex flex-col items-center">
+        <div className="home-navbar flex flex-col items-center bg-[#F7F4ED]">
             <div className="logo-div flex items-center justify-between w-full p-[25px] max-w-[1192px]">
                 <h1 className="font-noe text-black text-[#242424] text-4xl ">Medium</h1>
                 <div className="button-links flex">
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,21 @@
+"use client";
 import { Navbar } from "./components/navbar";
 import { Button } from "@/components/ui/button";
 import { Footer } from "./components/footer";
-const Home = () => {
-    return (
+import Image from "next/image";
+import { useRef } from "react";
+import { useScreenSize } from "./hooks/useScreenSize";
+import { cn } from "@/lib/utils";
 
+const Home = () => {
+    const imageRef = useRef<HTMLImageElement>(null)
+    const { width, height } = useScreenSize()
+
+    const ImageShift = (width: number) => {
+        return width > 1145 ? 0 : -(1145 - width);
+    };
+
+    return (
         <div className="home-page navbar" >
             <Navbar />
             <div className="homepage-section flex justify-center items-center w-screen h-[calc(100vh-(94px+68px))]">
@@ -15,6 +27,15 @@ const Home = () => {
                     <p className="font-inter text-xl">A place to read, write,and deepen your understanding</p>
                     <Button className="w-min px-6 py-6 font-bold text-base" variant={"rounded"}> Start reading</Button>
                 </div>
+                <Image
+                    ref={imageRef}
+                    className={cn("z-[-1] absolute")}
+                    style={{ right: `${ImageShift(width)}px` }}
+                    src={"/hero-image.webp"}
+                    height={500}
+                    width={500}
+                    alt="hero section image"
+                ></Image>
             </div>
             <Footer></Footer>
         </div>
