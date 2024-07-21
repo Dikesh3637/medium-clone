7b149a258ee0b97dec973d5c6f4e7d4ff5545629
Dikesh3637 <dikeshreddy.dev@gmail.com>
2024-07-21 12:59:05 +0530

    updated user.ts

--- packages/typing/src/user.ts
+++ packages/typing/src/user.ts
@@ -15,4 +15,17 @@ export const userSignInSchema = z.object({
 
 export type UserSignInType = z.infer<typeof userSignInSchema>
 
+export const UserSignUpFormSchema = z.object({
+    username: z.string().min(3, {
+        message: "Username must be atleast 3 characters long"
+    }).max(20, {
+        message: "Username must not exceed 20 characters"
+    }),
+    email: z.string().email({
+        message: "Invalid email address"
+    }),
+    password: z.string().min(6, {
+        message: "Password must be atleast 6 characters long"
+    }),
+})
 
